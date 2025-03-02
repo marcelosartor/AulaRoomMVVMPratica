552a803b5aded104d79535ea1a1e14a63cab73d9
marcelosartor <marcelo_sartor@hotmail.com>
2025-03-02 12:40:58 -0300

    feat: tests

--- app/src/androidTest/java/br/com/msartor/aularoommvvmpratica/data/dao/AnotacaoDaoTest.kt
+++ app/src/androidTest/java/br/com/msartor/aularoommvvmpratica/data/dao/AnotacaoDaoTest.kt
@@ -3,12 +3,20 @@ package br.com.msartor.aularoommvvmpratica.data.dao
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import br.com.msartor.aularoommvvmpratica.data.database.BancoDeDados
+import br.com.msartor.aularoommvvmpratica.data.entity.Anotacao
+import br.com.msartor.aularoommvvmpratica.data.entity.Categoria
+import com.google.common.truth.ExpectFailure
+import com.google.common.truth.Truth.assertThat
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 
 class AnotacaoDaoTest {
 
     private lateinit var bancoDeDados: BancoDeDados
+    private lateinit var categoriaDao: CategoriaDao
+    private lateinit var anotacaoDao: AnotacaoDao
 
     @Before
     fun setUp() {
@@ -16,6 +24,30 @@ class AnotacaoDaoTest {
             ApplicationProvider.getApplicationContext(),
             BancoDeDados::class.java
         ).allowMainThreadQueries().build()
+        categoriaDao = bancoDeDados.categoriaDao
+        anotacaoDao = bancoDeDados.anotacaoDao
+    }
+
+    @Test
+    fun salvarCategoria_verificaCategoriaCadastrada_retornaTrue() {
+        val categoria = Categoria(0,"mercado")
+        val idcategoria = categoriaDao.salvar(categoria)
+        assertThat( idcategoria).isGreaterThan(0L)
+    }
+
+    @Test
+    fun salvarAnotacao_verificaAnotacaoCadastrada_retornaTrue() {
+        salvarCategoria_verificaCategoriaCadastrada_retornaTrue()
+        val anotacao = Anotacao(0,"teste","teste",1)
+        val idAnotacao = anotacaoDao.insert(anotacao)
+        assertThat(idAnotacao).isGreaterThan(0L)
+    }
+
+    @Test
+    fun listarAnotacao_verificaListagemAnotacoes_retornaLista() {
+        salvarAnotacao_verificaAnotacaoCadastrada_retornaTrue()
+        val anotacoes = anotacaoDao.listarAnotacaoECategoria()
+        assertThat(anotacoes).isNotEmpty()
     }
 
     @After
